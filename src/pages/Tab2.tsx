1e73a1c754e380c36f862460c3bc0f86fab03244
Bhaskara Rao Potnuru <bhaskarpotnuru7095@gmail.com>
2024-04-25 15:57:44 +0530

    JSON Modification

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -9,7 +9,7 @@ const Tab2: React.FC = () => {
 const [data,setData] = useState(cartData)
 
   const handleRemove = (item) =>{
-    const updatedData = data.filter(each => each.id !== item.id);
+    const updatedData = data.filter(each => each.top_id !== item.top_id || each.length_id !== item.length_id);
     
     setData(updatedData)
   }
@@ -20,10 +20,10 @@ const [data,setData] = useState(cartData)
             <h1 className = "cart-heading">Cart</h1>
             {data.length === 0 ? <h1 className = "items-not-found-heading">Items Not Found</h1>:null}
 
-            {data.map(each =><div className = "cart-item-container" key = {each.id}>
+            {data.map(each =><div className = "cart-item-container" >
               <div className = "d-flex">
-                <img src = {each.imgUrl} alt = "cart-image" className = "cart-item-image" />
-                <p className = "cart-item-name">{each.id}</p>
+                <img src = {each.main_image} alt = "cart-image" className = "cart-item-image" />
+                {/* <p className = "cart-item-name">{each.top_id}</p> */}
                 </div>
                 <div>
                   <button className = "cart-item-remove-btn" onClick={() => handleRemove(each)}><MdDelete /></button>
